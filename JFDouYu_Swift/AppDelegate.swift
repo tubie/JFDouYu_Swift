ff7fb62ac1a64ea15af9881b5126f8fa93f18c9b
linjianfang <linjianfang@mminc-qa.onaliyun.com>
2020-08-21 12:11:09 +0800

    添加图片设置图片

--- JFDouYu_Swift/AppDelegate.swift
+++ JFDouYu_Swift/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UITabBar.appearance().tintColor = UIColor.orange
         // Override point for customization after application launch.
         return true
     }
